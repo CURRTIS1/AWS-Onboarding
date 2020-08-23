4632f5701d729bc3ff71a1f9313352107fbded06
unknown <curtiswallond@gmail.com>
2020-08-23 10:53:24 +0100

    update

--- Terraform/layers/000base/main.tf
+++ Terraform/layers/000base/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  version    = "~> 1.19"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  version    = "~> 3.3.0"
+  access_key = "var.aws_access_key"
+  secret_key = "var.aws_secret_key"
 }
