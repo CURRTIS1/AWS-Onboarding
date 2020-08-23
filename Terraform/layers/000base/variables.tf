8c1de473d6e44649e00a515ec0ef3f29423ca6d5
unknown <curtiswallond@gmail.com>
2020-08-23 11:02:20 +0100

    update

---
+++ Terraform/layers/000base/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "The region are building into."
+  type        = string
+}
+
+variable "environment" {
+  description = "Build environment"
+  type        = string
+}
+
+variable "layer" {
+  description = "Terraform layer"
+  type        = string
+}
