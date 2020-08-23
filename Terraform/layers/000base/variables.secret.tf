ff5f7bdb94c71e3f79c1f692c7a34ed709b79f57
unknown <curtiswallond@gmail.com>
2020-08-23 10:52:54 +0100

    update

--- Terraform/layers/000base/variables.secret.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  description = "The AWS Access Key"
-  default     = "AKIAI5DWAHAABMNUTZCQ"
-}
-
-variable "aws_secret_key" {
-  description = "The AWS Secret Key"
-  default     = "b7ZqIkCd8q524DOcX+3UWx+aDzrdNeG6a4eKDJhK"
-}
