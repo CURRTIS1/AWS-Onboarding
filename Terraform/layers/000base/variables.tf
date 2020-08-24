2eef13c6882793ed8e6f282eab4d474034650f87
unknown <curtiswallond@gmail.com>
2020-08-24 16:09:20 +0100

    update

--- Terraform/layers/000base/variables.tf
+++ Terraform/layers/000base/variables.tf
@@ -12,3 +12,24 @@ variable "layer" {
   description = "Terraform layer"
   type        = string
 }
+
+variable "instance_type" {
+  description = "The type of EC2 instance to use"
+  default = "t2.micro"
+}
+
+variable "autoscale_max" {
+  description = "The max number of ec2 instances in the asg"
+  default = 1
+}
+
+variable "autoscale_min" {
+  description = "The min number of ec2 instances in the asg"
+  default = 1
+}
+
+variable "alb_name" {
+  description = "The readable friendly name of the load balancer"
+  default = "my-alb"
+}
+
